7407b2b1b5ffdcc789d33c069b6c69bca06fccc2
Alexey_Chmutov <chmutov@ya.ru>
2017-09-26 15:55:13 +0300

    parallel provider fixed, xpath pattern replaced with css pattern

--- selenium-training/src/test/java/com/epam/chmutov/homework2/Task1Test.java
+++ selenium-training/src/test/java/com/epam/chmutov/homework2/Task1Test.java
@@ -2,33 +2,49 @@ package com.epam.chmutov.homework2;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
+import java.util.List;
+
 public class Task1Test {
     @DataProvider
-    public Object[][] xpathPatternAndExpectedTextProvider() {
+    private Object[][] expectedTextProvider() {
         return new Object[][]{
-                {"//span[text()=\"To include good practices\"]", "To include good practices\nand ideas from successful\nEPAM projec"},
-                {"//span[text()=\"To be flexible and\"]", "To be flexible and\ncustomizable"},
-                {"//span[text()=\"To be multiplatform \"]", "To be multiplatform"},
-                {"//span[text()=\"Already have good base\"]", "Already have good base\n(about 20 internal and\nsome external projects),\nwish to get more…"}};
+                {0, "To include good practices\nand ideas from successful\nEPAM projec"},
+                {1, "To be flexible and\ncustomizable"},
+                {2, "To be multiplatform"},
+                {3, "Already have good base\n(about 20 internal and\nsome external projects),\nwish to get more…"},
+        };
     }
 
-    @Test(dataProvider = "xpathPatternAndExpectedTextProvider")
-    public void testMainPageSpanTexts(String xpathPattern, String expectedText) {
+    @DataProvider(parallel = true)
+    private Object[][] parallelExpectedTextProvider() {
+        return expectedTextProvider();
+    }
+
+    @Test(dataProvider = "expectedTextProvider")
+    public void testMainPageSpanTexts(int elementIndex, String expectedText) {
         System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
-        WebDriver driver = new ChromeDriver();
-        driver.navigate().to("https://jdi-framework.github.io/tests");
-        Assert.assertEquals(driver.findElement(By.xpath(xpathPattern)).getText(), expectedText);
-        driver.close();
+        final WebDriver driver = new ChromeDriver();
+        try {
+            driver.navigate().to("https://jdi-framework.github.io/tests");
+            List<WebElement> benefitTxtElements = driver.findElements(By.cssSelector(".benefit-txt"));
+            if(benefitTxtElements.size() <= elementIndex) {
+                Assert.fail(".benefit-txt element #" + elementIndex + " not found");
+            }
+            Assert.assertEquals(benefitTxtElements.get(elementIndex).getText(), expectedText);
+        } finally {
+            driver.close();
+        }
     }
 
-    @Test(dataProvider = "xpathPatternAndExpectedTextProvider", threadPoolSize = 2, invocationCount = 2)
-    public void testMainPageSpanTextsInParallel(String xpathPattern, String expectedText) {
-        testMainPageSpanTexts(xpathPattern, expectedText);
+    @Test(dataProvider = "parallelExpectedTextProvider")
+    public void testMainPageSpanTextsInParallel(int elementIndex, String expectedText) {
+        testMainPageSpanTexts(elementIndex, expectedText);
     }
 
 }
